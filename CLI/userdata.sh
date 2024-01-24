97b7ab46eea0e2b046d7988da2a9bea565f10cc7
Murat Yenidoğan <151568549+muratyenidogan@users.noreply.github.com>
2024-01-24 19:45:58 +0300

    Forth Commit

--- CLI/userdata.sh
+++ CLI/userdata.sh
@@ -5,9 +5,9 @@ yum install python3 -y
 yum install python3-pip -y
 pip3 install flask
 yum install git -y
-FOLDER="https://raw.githubusercontent.com/awsdevopsteam/roman-number-conventor/master/templates"
+FOLDER="https://raw.githubusercontent.com/muratyenidogan/Roman-Numerals-Converter/main/templates"
 cd /home/ec2-user
 wget -P templates ${FOLDER}/index.html
 wget -P templates ${FOLDER}/result.html
-wget https://raw.githubusercontent.com/awsdevopsteam/roman-number-conventor/master/roman-numerals-converter-app.py
+wget https://raw.githubusercontent.com/muratyenidogan/Roman-Numerals-Converter/main/roman-numerals-converter-app.py
 python3 roman-numerals-converter-app.py
